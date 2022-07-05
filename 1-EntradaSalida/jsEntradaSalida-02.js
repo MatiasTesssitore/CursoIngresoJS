f56e8212c1c021da9542071c7d5c952239854956
Mati Tessitore <matiastessitore.4a@gmail.com>
2022-07-04 23:05:16 -0300

    mensaje

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,26 @@
 /*
+Matias Tessitore 
+Div E 
+Ejercicio 2 E/S 
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
 {
-	alert("ok");
-}
+ //declaro una variable
+let nombre;
 
+//tomar datos 
+//    matias 
+//nombre=matias
+nombre = prompt ("Introduzca nombre"); 
+
+//mostrar informacion 
+	alert("su nombre es");
+
+	alert(nombre); 
+
+
+	//mostrar informacion
+
+}
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,39 @@
 /*
+Matias Tessitore 
+Div E
+Ej 3 E/S
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+
+
+
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	//declarar variable
+    let nombreIngresado; 
+
+    //tomar datos 
+	//inicializar variable 
+    nombreIngresado = document.getElementById("txtIdNombre").value;
+
+
+//mostrar informacion 
+alert(nombreIngresado); 
+
 
 }
 
 
+
+/*
+//ingresamos datos 
+-prompt("mensaje")
+-document.getElementById("txtIdNombre").value; 
+
+//procesamos datos 
+
+
+
+//mostramos datos 
+-alert("mensaje" o "variable ")
+*/
